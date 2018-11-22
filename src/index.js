6a2f776febf97cf94a34dcaf79d56927bc0c6192
intuitive-programmer <steven@intuitiveprogrammer.com>
2018-11-22 11:18:42 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,29 @@
-const fullname = document.getElementById("fullname");
-console.log("CONTENT NOT YET LOADED!", fullname); //what will fullname evaluate to?
+const addPersonButton = document.getElementsByClassName("btn-primary")[0]
 
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("CONTENT LOADED!");
-});
+addPersonButton.addEventListener('click', fetchUser)
+
+function fetchUser() {
+  
+  fetch('https://randomuser.me/api/')
+    .then(res => res.json())
+    .then(render)
+
+   function render(response){
+      const userData = response.results[0]
+      console.log(userData)
+        document.getElementById("fullname").innerText = `${capitalise(userData.name.first)} ${capitalise(userData.name.last)}`
+        document.getElementById("email").innerText = userData.email
+        document.getElementById("street").innerText = userData.location.street
+        document.getElementById("city").innerText = userData.location.city
+        document.getElementById("state").innerText = userData.location.state
+        document.getElementById("postcode").innerText = userData.location.postcode
+        document.getElementById("phone").innerText = userData.phone
+        document.getElementById("cell").innerText = userData.cell
+        document.getElementById("date_of_birth").innerText = userData.dob.date
+        document.getElementById("profile_picture").src = userData.picture.thumbnail
+    }
+
+    function capitalise(word) {
+      return word[0].toUpperCase() + word.substr(1)
+    }
+}
